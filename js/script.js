6b1774764200ebb5074dfb60309721a1cfb106af
eddyK15501 <itgma1231@gmail.com>
2023-06-14 22:45:00 -0400

    added go back and high scores link

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ const fScore = document.getElementById('final-score')
 const hsInitials = document.getElementById('fs')
 const submitBtn = document.getElementById('submit-hs')
 const highScoresPage = document.querySelector('.high-scores-page')
+const highScoresList = document.querySelector('.high-scores-list')
+const goBack = document.getElementById('go-back')
+const clearHs = document.getElementById('clear')
+const goToHsPage = document.querySelector('a')
 
 let finalScore = 0
 let gameOver = false
@@ -30,8 +34,8 @@ const startTimeCount = () => {
         // if the game is over
         if (gameOver) {
             clearInterval(timer)
-            showScore()
         }
+
         // if the timer goes to zero
         if (totalTime < 0) {
             clearInterval(timer)
@@ -55,13 +59,13 @@ const startQuiz = () => {
 const showQuestion = (index) => {
     const question = document.getElementById('question')
     const answers = document.getElementById('answers')
-    
+
     // if there are still questions left that are able to be displayed
     if (questionIndex <= triviaQuestions.length - 1) {
         question.innerHTML = `<h1>${triviaQuestions[index].question}</h1>`
-    
-        answers.innerHTML = 
-        `<button class="btn">
+
+        answers.innerHTML =
+            `<button class="btn">
         ${triviaQuestions[index].options[0]}
         </button>
         <button class="btn">
@@ -75,25 +79,26 @@ const showQuestion = (index) => {
         </button>`
 
         const buttons = document.querySelectorAll('.btn')
-    
+
         buttons.forEach(button => {
             button.addEventListener('click', checkAnswer)
         })
     } else {
+        gameOver = true
         showScore()
-    } 
+    }
 }
 
 // check if answer is correct or wrong
 const checkAnswer = (event) => {
-    
+
     const answerClicked = event.target.innerText
 
     // if the answer is correct
     if (answerClicked === triviaQuestions[questionIndex].answer) {
         correctOrWrong.innerText = 'Correct!'
         answerBar.classList.remove('hide')
-    // if the answer is wrong
+        // if the answer is wrong
     } else if (answerClicked !== triviaQuestions[questionIndex].answer) {
         totalTime -= 10
         correctOrWrong.innerText = 'Wrong!'
@@ -102,7 +107,7 @@ const checkAnswer = (event) => {
         gameOver = true
         showScore()
     }
-    
+
     // display correct or wrong for 500 milliseconds after answering
     setTimeout(() => {
         answerBar.classList.add('hide')
@@ -130,30 +135,97 @@ const createHighScore = () => {
     let hs = {
         gimmeInitials,
         finalScore
-    } 
-
-    console.log(hs)
+    }
     
+    highScoresArray.push(hs)
+    
+    saveHighScore()
+    displayHighScores()
+
+    // let loadHighScores = JSON.parse(localStorage.getItem('HighScore'))
+
+    // let lastHighScore = loadHighScores.slice(-1)
    
-   
-   
-   
-   
-   
+    // let highScoreLi = document.createElement('li')
+    //     highScoreLi.innerText = `${highScoresArray.length}. ${lastHighScore[0].gimmeInitials} - ${lastHighScore[0].finalScore}`
+    //     highScoresList.appendChild(highScoreLi)
+
+
+
     resultsPage.classList.add('hide')
     highScoresPage.classList.remove('hide')
-    
+
 }
 
+const saveHighScore = () => {
+    localStorage.setItem('HighScore', JSON.stringify(highScoresArray))
+}
 
+const displayHighScores = (e) => {
+    e.preventDefault()  
+
+    gameOver = true
+   
+    resultsPage.classList.add('hide')
+    startPage.classList.add('hide')
+    questionsPage.classList.add('hide')
+   
+    highScoresPage.classList.remove('hide')
+    timerLeft.innerText = `Time: 0`
+}
+
+const getHighScore = () => {
+    let loadHighScores = JSON.parse(localStorage.getItem('HighScore'))
+
+    if (!loadHighScores) {
+        return false
+    }
+
+    let lastHighScore = loadHighScores.slice(-1)
+   
+    let highScoreLi = document.createElement('li')
+        highScoreLi.innerText = `${loadHighScores.length}. ${lastHighScore[0].gimmeInitials} - ${lastHighScore[0].finalScore}`
+        highScoresList.appendChild(highScoreLi)
+
+    // highScoresArray.push(lastHighScore)
+
+    // loadHighScores.forEach((hs, index) => {
+    //     let highScoreLi = document.createElement('li')
+    //     highScoreLi.innerText = `${index + 1}. ${hs.gimmeInitials} - ${hs.finalScore}`
+    //     highScoresList.appendChild(highScoreLi)
+
+    //     highScoresArray.push(hs)
+    // })
+
+
+}
+
+const goBackToStart = () => {
+    
+    gameOver = false
+    questionIndex = 0
+    totalTime = 75
+    finalScore = 0
+    gimmeInitials = ''
+
+    renderStartPage()
+}
+
+const renderStartPage = () => {
+    highScoresPage.classList.add('hide')
+    startPage.classList.remove('hide')
+    timerLeft.innerText = `Time: 0`
+}
+
+getHighScore()
 
 
 
 // add event listeners
 startButton.addEventListener('click', startQuiz)
 
-// hsInitials.addEventListener('keydown', (e) => {
-//     console.log(e.target.value)
-// })
-
 submitBtn.addEventListener('click', createHighScore)
+
+goBack.addEventListener('click', goBackToStart)
+
+goToHsPage.addEventListener('click', displayHighScores)
